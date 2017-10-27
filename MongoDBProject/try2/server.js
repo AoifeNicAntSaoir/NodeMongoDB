16ff361e21a1662dc45184f87c8f80c1f74429ae
Aoife <aoifesayers@gmail.com>
2017-10-28 00:28:29 +0100

    Removed Mongoose Schema - Now working with Express Express insert and read

---
+++ MongoDBProject/try2/server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+var db
+
+MongoClient.connect('mongodb://localhost:27017/exampleDb', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  db.collection('flightDetails').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {flightDetails: result})
+  })
+})
+
+app.post('/flightDetails', (req, res) => {
+  db.collection('flightDetails').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+/*
+app.put('/flightDetails', (req, res) => {
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+
+
+app.delete('/flightDetails', (req, res) => {
+  db.collection('flightDetails').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('A darth vadar quote got deleted')
+  db.collection('flightDetails')
+  })
+})
+*/
