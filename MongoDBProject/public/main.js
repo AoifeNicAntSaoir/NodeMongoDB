0e1d2f6c366b367c094a063b6578f15ef4114841
Aoife <aoifesayers@gmail.com>
2017-11-02 15:53:56 +0000

    Still working update Update on document and more to the conclusion Comments on code

--- MongoDBProject/public/main.js
+++ MongoDBProject/public/main.js
@@ -1,18 +1,22 @@
 /* globals fetch */
 var update = document.getElementById('update')
+var newUp
+var fieldToUpdate = document.getElementById('oldNo')
+var fieldUpdatedTo = document.getElementById('newNo')
 var del = document.getElementById('delete')
 var passportDel = document.getElementById('nameToDelete')
-var key
-var valueF
+
 
 update.addEventListener('click', function () {
-	key = document.getElementById('keyField').value
-	valueF = document.getElementById('valueField')
+	fieldToUpdate = document.getElementById('oldNo').value
+	fieldUpdatedTo = document.getElementById('newNo').value
+	alert(fieldToUpdate)
+	alert(fieldUpdatedTo)
   fetch('flightDetails', {
     method: 'put',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
-      key: valueF
+      'passportNo': fieldUpdatedTo
     })
   })
   .then(response => {
@@ -20,6 +24,7 @@ update.addEventListener('click', function () {
   })
   .then(data => {
     console.log(data)
+	window.location.reload()
   })
 })
 
--- MongoDBProject/server.js
+++ MongoDBProject/server.js
@@ -1,3 +1,4 @@
+//Imports
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,6 +6,7 @@ const MongoClient = require('mongodb').MongoClient
 
 var db
 
+//Mongo Client connection and listening on localhost:3000
 MongoClient.connect('mongodb://localhost:27017/exampleDb', (err, database) => {
   if (err) return console.log(err)
   db = database
@@ -18,13 +20,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
+//Read from flightDetails collection and show results in index.ejs template
 app.get('/', (req, res) => {
   db.collection('flightDetails').find().toArray((err, result) => {
     if (err) return console.log(err)
     res.render('index.ejs', {flightDetails: result})
   })
 })
-
+//Create a document in flightDetails - takes data from html form in index.ejs
 app.post('/flightDetails', (req, res) => {
   db.collection('flightDetails').save(req.body, (err, result) => {
     if (err) return console.log(err)
@@ -33,11 +36,12 @@ app.post('/flightDetails', (req, res) => {
   })
 })
 
+//Update & sort
 app.put('/flightDetails', (req, res) => {
   db.collection('flightDetails')
-  .findOneAndUpdate({passportNo: 'Yoda'}, {
+  .findOneAndUpdate({passportNo: req.body.passportNo}, {
     $set: {
-      passportNo: req.body.passportNo
+      'passportNo': req.body.passportNo
     }
   }, {
     sort: {_id: -1},
@@ -47,7 +51,7 @@ app.put('/flightDetails', (req, res) => {
     res.send(result)
   })
 })
-
+//Delete collection by passport no
 app.delete('/flightDetails', (req, res) => {
   db.collection('flightDetails').findOneAndDelete({passportNo: req.body.passportNo}, (err, result) => {
     if (err) return res.send(500, err)
